70c97c7aee81b779e2d2ea740eefeacf551ad8e6
Maria Malysheva <mn.malysheva@mail.ru>
2021-08-16 15:40:53 +0300

    Generate operators supporting multidirectional broadcasting

--- core/src/jvmMain/kotlin/io/kinference/compiler/generation/operators/OperatorGenerator.kt
+++ core/src/jvmMain/kotlin/io/kinference/compiler/generation/operators/OperatorGenerator.kt
@@ -2,11 +2,19 @@ package io.kinference.compiler.generation.operators
 
 import com.squareup.kotlinpoet.CodeBlock
 import io.kinference.compiler.generation.operators.common.DefaultOperatorGenerator
+import io.kinference.compiler.generation.operators.flow.WhereGenerator
+import io.kinference.compiler.generation.operators.logical.EqualGenerator
+import io.kinference.compiler.generation.operators.logical.GreaterGenerator
+import io.kinference.compiler.generation.operators.logical.OrGenerator
 import io.kinference.compiler.generation.operators.math.AddGenerator
 import io.kinference.compiler.generation.operators.math.MatMulGenerator
 import io.kinference.compiler.generation.operators.math.MulGenerator
 import io.kinference.compiler.generation.operators.math.SubGenerator
 import io.kinference.operators.Operator
+import io.kinference.operators.flow.Where
+import io.kinference.operators.logical.Equal
+import io.kinference.operators.logical.Greater
+import io.kinference.operators.logical.Or
 import io.kinference.operators.math.Add
 import io.kinference.operators.math.MatMul
 import io.kinference.operators.math.Mul
@@ -26,9 +34,13 @@ class OperatorGenerator(
     override fun generate(): CodeBlock =
         when (operator) {
             is Add -> AddGenerator(operator, info).generate()
+            is Equal -> EqualGenerator(operator, info).generate()
+            is Greater -> GreaterGenerator(operator, info).generate()
             is MatMul -> MatMulGenerator(operator, info).generate()
             is Mul -> MulGenerator(operator, info).generate()
+            is Or -> OrGenerator(operator, info).generate()
             is Sub -> SubGenerator(operator, info).generate()
+            is Where -> WhereGenerator(operator, info).generate()
             else -> DefaultOperatorGenerator(operator, info).generate()
         }
 }
