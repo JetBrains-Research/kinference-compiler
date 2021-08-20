013c800511e0300555c76fec93337d8cf53baef5
Maria Malysheva <mn.malysheva@mail.ru>
2021-08-20 11:12:05 +0300

    Support prepared tensors

--- core/src/jvmMain/kotlin/io/kinference/compiler/generation/operators/layer/recurrent/GRUGenerator.kt
+++ core/src/jvmMain/kotlin/io/kinference/compiler/generation/operators/layer/recurrent/GRUGenerator.kt
@@ -3,6 +3,8 @@ package io.kinference.compiler.generation.operators.layer.recurrent
 import io.kinference.compiler.generation.info.TensorInfo
 import io.kinference.compiler.generation.operators.OperatorGenerationInfo
 import io.kinference.compiler.generation.operators.common.TypeAndShapeAwareOperatorGenerator
+import io.kinference.data.tensors.Tensor
+import io.kinference.data.tensors.asTensor
 import io.kinference.operators.layer.recurrent.gru.GRU
 import kotlin.time.ExperimentalTime
 
@@ -25,6 +27,31 @@ class GRUGenerator(
     private val linearBeforeReset: Boolean = operator.getAttribute<Number>("linear_before_reset").toInt() == 1
 
     private val numLayers = if (direction == "bidirectional") 2 else 1
+
+    private fun prepareWeights(tensor: Tensor): Tensor {
+        val shape = tensor.data.shape
+        val newShape = intArrayOf(shape[0], 3, shape[1] / 3, shape[2])
+        return tensor.data.reshapeView(newShape).toMutable().transpose(intArrayOf(0, 1, 3, 2)).asTensor("prepared_${tensor.info.name}")
+    }
+
+    private fun prepareBias(tensor: Tensor): Tensor {
+        val shape = tensor.data.shape
+        val newShape = intArrayOf(shape[0], 6, shape[1] / 6)
+        return tensor.data.toMutable().reshape(newShape).asTensor("prepared_${tensor.info.name}")
+    }
+
+    init {
+        val weights = tensorInfo[operator.inputs[1]]
+        val recurrentWeights = tensorInfo[operator.inputs[2]]
+        val bias = tensorInfo[operator.inputs.getOrNull(3)]
+        listOfNotNull(
+            weights?.tensor?.let { prepareWeights(it) },
+            recurrentWeights?.tensor?.let { prepareWeights(it) },
+            bias?.tensor?.let { prepareBias(it) }
+        ).forEach {
+            preparedContextBuilder.addTensor(it.info.name, it)
+        }
+    }
 
     override fun resultInfo(): Map<String, TensorInfo> {
         val input = tensorInfo.getValue(operator.inputs.first())
